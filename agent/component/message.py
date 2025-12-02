8bc2e77081f72c4de8a8815fe10076e371db8426
buua436 <sz_buua@foxmail.com>
2025-12-02 10:52:57 +0800

    update

--- agent/component/message.py
+++ agent/component/message.py
@@ -41,7 +41,7 @@ class MessageParam(ComponentParamBase):
         self.content = []
         self.stream = True
         self.output_format = None  # default output format
-        self.need_TTS = False
+        self.auto_play = False
         self.outputs = {
             "content": {
                 "type": "str"
